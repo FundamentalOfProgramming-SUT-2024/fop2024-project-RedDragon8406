2a54738b027243eb23ef805ff78b2b902aa80eaf
sepica <sepehr.kardel@gmail.com>
2025-01-01 23:40:59 +0330

    made the basics of signup window

--- auth.c
+++ auth.c
@@ -1,9 +1,14 @@
 #include <ncurses.h>
+#include <stdio.h>
 #include <string.h>
+#include <unistd.h>
+#include <regex.h>
+#include <ctype.h>
 #include "auth.h"
 #include "main.h"
 #include "menu.h"
 #include "design.h"
+
 
 void authentication_window() {
     WINDOW *auth_win;
@@ -91,10 +96,11 @@ void authentication_window() {
                 getch();
                 break;
             } else if (choice == 2) { // Signup
-                mvprintw(LINES - 2, 0, "Signup selected\n");
+                delwin(auth_win);
+                clear();
                 refresh();
-                getch();
-                break;
+                signup_window();
+                return;
             } else if (choice == 3) { // Continue as Guest
                 refresh();
                 delwin(auth_win);
@@ -107,3 +113,117 @@ void authentication_window() {
     }
     endwin();
 }
+
+void signup_window() {
+    WINDOW *signup_win;
+    int height = 17;
+    int width = 50;
+    int starty = (LINES - height) / 2;
+    int startx = (COLS - width) / 2;
+
+    signup_win = newwin(height, width, starty, startx);
+    keypad(signup_win, TRUE);
+    box(signup_win, 0, 0);
+
+    const char *title = "Sign Up";
+    mvwprintw(signup_win, 1, (width - strlen(title)) / 2, "%s", title);
+
+    const char *username_label = "Username:";
+    const char *password_label = "Password:";
+    const char *email_label = "Email:";
+
+    mvwprintw(signup_win, 3, 2, "%s", username_label);
+    mvwprintw(signup_win, 5, 2, "%s", password_label);
+    mvwprintw(signup_win, 7, 2, "%s", email_label);
+
+    const char *back_button = "Back";
+    const char *signup_button = "Sign Up";
+
+    mvwprintw(signup_win, 10, (width - strlen(back_button)) / 2, "%s", back_button);
+    mvwprintw(signup_win, 12, (width - strlen(signup_button)) / 2, "%s", signup_button);
+
+    char username[MAX_USERNAME_LENGTH] = "";
+    char password[MAX_PASSWORD_LENGTH] = "";
+    char email[MAX_EMAIL_LENGTH] = "";
+    int highlight = 1;
+    int c;
+
+    while (1) {
+        if (highlight == 1) {
+            wattron(signup_win, A_REVERSE);
+            mvwprintw(signup_win, 3, 12, "%-30s", username);
+            wattroff(signup_win, A_REVERSE);
+        } else {
+            mvwprintw(signup_win, 3, 12, "%-30s", username);
+        }
+
+        if (highlight == 2) {
+            wattron(signup_win, A_REVERSE);
+            mvwprintw(signup_win, 5, 12, "%-30s", password);
+            wattroff(signup_win, A_REVERSE);
+        } else {
+            mvwprintw(signup_win, 5, 12, "%-30s", password);
+        }
+
+        if (highlight == 3) {
+            wattron(signup_win, A_REVERSE);
+            mvwprintw(signup_win, 7, 12, "%-30s", email);
+            wattroff(signup_win, A_REVERSE);
+        } else {
+            mvwprintw(signup_win, 7, 12, "%-30s", email);
+        }
+
+        if (highlight == 4) {
+            wattron(signup_win, A_REVERSE);
+            mvwprintw(signup_win, 10, (width - strlen(back_button)) / 2, "%s", back_button);
+            wattroff(signup_win, A_REVERSE);
+        } else {
+            mvwprintw(signup_win, 10, (width - strlen(back_button)) / 2, "%s", back_button);
+        }
+
+        if (highlight == 5) {
+            wattron(signup_win, A_REVERSE);
+            mvwprintw(signup_win, 12, (width - strlen(signup_button)) / 2, "%s", signup_button);
+            wattroff(signup_win, A_REVERSE);
+        } else {
+            mvwprintw(signup_win, 12, (width - strlen(signup_button)) / 2, "%s", signup_button);
+        }
+
+        wrefresh(signup_win);
+        c = wgetch(signup_win);
+
+        switch (c) {
+            case KEY_UP:
+                if (highlight == 1)
+                    highlight = 5;
+                else
+                    --highlight;
+                break;
+            case KEY_DOWN:
+                if (highlight == 5)
+                    highlight = 1;
+                else
+                    ++highlight;
+                break;
+            case 10:
+                if (highlight == 4) { // back button
+                    delwin(signup_win);
+                    clear();
+                    refresh();
+                    authentication_window();
+                    return;
+                } else if (highlight == 5) {
+                    // signup
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    delwin(signup_win);
+    clear();
+    refresh();
+    show_main_menu();
+}
+
--- auth.h
+++ auth.h
@@ -1,6 +1,16 @@
 #ifndef AUTH_H
 #define AUTH_H
 
+#define MAX_USERNAME_LENGTH 50
+#define MAX_PASSWORD_LENGTH 50
+#define MAX_EMAIL_LENGTH 100
+#define USERS_DIR "users/"
+
+// Function prototypes
+void create_user_file(const char* username, const char* password, const char* email);
+int user_exists(const char* username);
+int validate_email(const char* email);
+void signup_window();
 void authentication_window();
 
 #endif
